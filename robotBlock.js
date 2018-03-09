d951986b5dc7af5bb9e4cd82eaf701c35e380fb5
brcyprod <brice.brcy@gmail.com>
2018-03-09 15:52:46 +0100

    Update robotBlock.js

--- robotBlock.js
+++ robotBlock.js
@@ -856,27 +856,27 @@ Blockly.Blocks['if_touche_telecommande'] = {
 
 
 
-//Blockly.Blocks['controls_if_if'] = {
+Blockly.Blocks['controls_if_if'] = {
   /**
    * Mutator block for if container.
    * @this Blockly.Block
    */
- /* init: function() {
+  init: function() {
     this.setColour(210);
     this.appendDummyInput()
         .appendField("test 1");
-    this.appendStatementInput('STACK');
+    //this.appendStatementInput('STACK');
     this.setTooltip("test 2");
     this.contextMenu = false;
   }
 };
 
-Blockly.Blocks['controls_if_elseif'] = {*/
+Blockly.Blocks['controls_if_elseif'] = {
   /**
    * Mutator bolck for else-if condition.
    * @this Blockly.Block
    */
-  /*init: function() {
+  init: function() {
     this.setColour(210);
     this.appendDummyInput()
         .appendField("test 3");
@@ -887,12 +887,12 @@ Blockly.Blocks['controls_if_elseif'] = {*/
   }
 };
 
-Blockly.Blocks['controls_if_telecommande'] = {*/
+Blockly.Blocks['controls_if_telecommande'] = {
   /**
    * Block for if/elseif/else condition.
    * @this Blockly.Block
    */
- /* init: function() {
+  init: function() {
     this.setHelpUrl("test 5");
     this.setColour(210);
     this.appendValueInput('IF0')
@@ -920,13 +920,13 @@ Blockly.Blocks['controls_if_telecommande'] = {*/
     });
     this.elseifCount_ = 0;
     this.elseCount_ = 0;
-  },*/
+  },
   /**
    * Create XML to represent the number of else-if and else inputs.
    * @return {Element} XML storage element.
    * @this Blockly.Block
    */
- /* mutationToDom: function() {
+  mutationToDom: function() {
     if (!this.elseifCount_ && !this.elseCount_) {
       return null;
     }
@@ -938,13 +938,13 @@ Blockly.Blocks['controls_if_telecommande'] = {*/
       container.setAttribute('else', 1);
     }
     return container;
-  },*/
+  },
   /**
    * Parse XML to restore the else-if and else inputs.
    * @param {!Element} xmlElement XML storage element.
    * @this Blockly.Block
    */
-  /*domToMutation: function(xmlElement) {
+  domToMutation: function(xmlElement) {
     this.elseifCount_ = parseInt(xmlElement.getAttribute('elseif'), 10) || 0;
     this.elseCount_ = parseInt(xmlElement.getAttribute('else'), 10) || 0;
     for (var i = 1; i <= this.elseifCount_; i++) {
@@ -958,14 +958,14 @@ Blockly.Blocks['controls_if_telecommande'] = {*/
       this.appendStatementInput('ELSE')
           .appendField("test 14");
     }
-  },*/
+  },
   /**
    * Populate the mutator's dialog with this block's components.
    * @param {!Blockly.Workspace} workspace Mutator's workspace.
    * @return {!Blockly.Block} Root block in mutator.
    * @this Blockly.Block
    */
-  /*decompose: function(workspace) {
+  decompose: function(workspace) {
     var containerBlock = Blockly.Block.obtain(workspace, 'controls_if_if');
     containerBlock.initSvg();
     var connection = containerBlock.getInput('STACK').connection;
@@ -981,13 +981,13 @@ Blockly.Blocks['controls_if_telecommande'] = {*/
       connection.connect(elseBlock.previousConnection);
     }
     return containerBlock;
-  },*/
+  },
   /**
    * Reconfigure this block based on the mutator dialog's components.
    * @param {!Blockly.Block} containerBlock Root block in mutator.
    * @this Blockly.Block
    */
-  /*compose: function(containerBlock) {
+  compose: function(containerBlock) {
     // Disconnect the else input blocks and remove the inputs.
     if (this.elseCount_) {
       this.removeInput('ELSE');
@@ -1033,13 +1033,13 @@ Blockly.Blocks['controls_if_telecommande'] = {*/
       clauseBlock = clauseBlock.nextConnection &&
           clauseBlock.nextConnection.targetBlock();
     }
-  },*/
+  },
   /**
    * Store pointers to any connected child blocks.
    * @param {!Blockly.Block} containerBlock Root block in mutator.
    * @this Blockly.Block
    */
- /* saveConnections: function(containerBlock) {
+  saveConnections: function(containerBlock) {
     var clauseBlock = containerBlock.getInputTargetBlock('STACK');
     var i = 1;
     while (clauseBlock) {
@@ -1067,12 +1067,12 @@ Blockly.Blocks['controls_if_telecommande'] = {*/
   }
 };
 
-Blockly.Blocks['controls_if_else'] = {*/
+Blockly.Blocks['controls_if_else'] = {
   /**
    * Mutator block for else condition.
    * @this Blockly.Block
    */
-  /*init: function() {
+  init: function() {
     this.setColour(210);
     this.appendDummyInput()
         .appendField("test 18");
@@ -1080,4 +1080,4 @@ Blockly.Blocks['controls_if_else'] = {*/
     this.setTooltip("test 19");
     this.contextMenu = false;
   }
-};*/
+};
