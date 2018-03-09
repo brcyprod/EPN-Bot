51803f04015d2ca0afa498fbfccd46fba5fdd14a
brcyprod <brice.brcy@gmail.com>
2018-03-09 16:51:01 +0100

    Update robotGenerator.js

--- robotGenerator.js
+++ robotGenerator.js
@@ -410,7 +410,7 @@ Blockly.Arduino['controls_if_telecommande'] = function(block) {
   // If/elseif/else condition.
   var n = 0;
   var argument = Blockly.Arduino.valueToCode(this, 'id_telecommande',
-      Blockly.Arduino.ORDER_NONE) || 'false';
+      Blockly.Arduino.ORDER_NONE) || 'false2';
   var branch = Blockly.Arduino.statementToCode(this, 'DO' + n);
   var code = 'if (' + argument + ') {\n' + branch + '\n}';
   for (n = 1; n <= this.elseifCount_; n++) {
